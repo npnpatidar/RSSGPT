5f7533ba95a6939a5f69e56e2cc239e793438ac0
npnpatidar <7de6dkm1@duck.com>
2023-10-13 11:17:15 +0530

    feeds updated and error during sorting fixed

--- feed.py
+++ feed.py
@@ -153,15 +153,15 @@ def update_summary_for_all_tables(pool, g4f):
             table_name = table_name[0]
 
             cursor.execute(f'''
-                SELECT entry_id, entry_url, entry_article_text, entry_title
+                SELECT entry_id, entry_url, entry_article_text, entry_title, entry_date
                 FROM {table_name}
                 WHERE entry_summary = "NO SUMMARY"
             ''')
 
             entries_with_no_summary = cursor.fetchall()
-            entries_with_no_summary = sorted(entries_with_no_summary, key=lambda x: datetime.strptime(x[6], '%a, %d %b %Y %H:%M:%S %z'), reverse=True)
+            entries_with_no_summary = sorted(entries_with_no_summary, key=lambda x: datetime.strptime(x[4], '%a, %d %b %Y %H:%M:%S %z'), reverse=True)
 
-            for entry_id, entry_url, entry_article_text, entry_title in entries_with_no_summary:
+            for entry_id, entry_url, entry_article_text, entry_title, entry_date in entries_with_no_summary:
                 executor.submit(summarize_and_update,pool, g4f, table_name, entry_id, entry_article_text, entry_title)
         pool.release_connection(connection)
 
